f91a6ed727390653d8790f6030f53bf334f01abb
Yuu Konno <yuu.developer@gmail.com>
2024-05-13 20:46:21 +0900

    記事の更新日無しケースのため、コンポーネントとCSSを調整

--- src/components/DateHeader/index.tsx
+++ src/components/DateHeader/index.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './date-header.css';
 
 const DateHeader = ({created, updated}) => {
+  const createdDate = created;
+  const updatedDate = updated ? updated : `Unchanged`;
   return (
-    <div className="content-dates">
-      <div className="content-date-entry">
-        <div className="content-date-label">Created At:</div>
-        <div className="date">{created}</div>
+    <div className="dates-container">
+      <div className="date-entry">
+        <div className="date-label">Created At:</div>
+        <div className="date-value">{createdDate}</div>
       </div>
-      <div className="content-date-entry">
-        <div className="content-date-label">Updated At:</div>
-        <div className="date">{updated}</div>
+      <div className="date-entry">
+        <div className="date-label">Updated At:</div>
+        <div className="date-value">{updatedDate}</div>
       </div>
     </div>
   );
